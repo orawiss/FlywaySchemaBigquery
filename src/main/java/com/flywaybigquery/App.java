e98eab82b154b49f58af88e05579bb3098f4d4a9
orawiss <kiwan2005@gmail.com>
2023-01-07 16:31:19 +0100

    commit

---
+++ src/main/java/com/flywaybigquery/App.java
@@ -0,0 +1,23 @@
+package com.flywaybigquery;
+import com.simba.googlebigquery.jdbc42.DataSource;
+import org.flywaydb.core.Flyway;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+
+public class App {
+    public static void main(String[] args) {
+        DataSource dataSource = new DataSource();
+        dataSource.setURL("jdbc:bigquery://https://www.googleapis.com/bigquery/v2:443;ProjectId=data-product-analytic-ecommrce;OAuthType=3");
+
+        Flyway flyway = Flyway.configure()
+                .createSchemas(false)
+                .defaultSchema("data-product-analytic-ecommrce.flyway")
+                .schemas("data-product-analytic-ecommrce.flyway")
+                .dataSource(dataSource)
+                .baselineOnMigrate(true)
+                .load();
+
+        flyway.migrate();
+    }
+}
