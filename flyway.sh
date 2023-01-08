398a144d73b83031ea753329e1f80bee451df90e
Wissem <kiwan2005@gmail.com>
2023-01-08 10:23:44 +0100

    Update flyway.sh

--- flyway.sh
+++ flyway.sh
@@ -1,5 +1,6 @@
 set -e
 /usr/local/bin/jq -n "$DBT_SA" > /tmp/credentials.json
+echo "$DBT_SA"
 export KEYFILE=/tmp/credentials.json
 export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC42/1.0/GoogleBigQueryJDBC42-1.0.jar
 java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar
