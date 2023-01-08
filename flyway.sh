f11ee98c97df17eae5dcc00b5007fbae68295e87
orawiss <kiwan2005@gmail.com>
2023-01-08 13:39:33 +0100

    added artifact jar file

--- flyway.sh
+++ flyway.sh
@@ -4,4 +4,4 @@ echo "$FLYWAY_SA"
 export KEYFILE=/tmp/credentials.json
 export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC42/1.0/GoogleBigQueryJDBC42-1.0.jar
 ls -rtl /FlywaySchemaBigquery/flyway/db/migration/
-java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar "filesystem:/FlywaySchemaBigquery/flyway/db/migration/"
+java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar filesystem:/FlywaySchemaBigquery/flyway/db/migration/
