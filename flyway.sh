c7b6efdf8602fa4ada3d504717d72b251887048c
Wissem <kiwan2005@gmail.com>
2023-01-08 12:48:53 +0100

    Update flyway.sh

--- flyway.sh
+++ flyway.sh
@@ -6,5 +6,5 @@ export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC
 mkdir -p /FlywaySchemaBigquery/target/db/migration/
 ls -rtl /FlywaySchemaBigquery/src/main/resources/db/migration/
 #java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar "filesystem:/FlywaySchemaBigquery/target/db/migration/"
-cp -R /FlywaySchemaBigquery/src/main/resources/db/migration/ /FlywaySchemaBigquery/target/db/migration/
+cp -R /FlywaySchemaBigquery/src/main/resources/db/migration/* /FlywaySchemaBigquery/target/db/migration/*
 ls -rtl /FlywaySchemaBigquery/target/db/migration/
