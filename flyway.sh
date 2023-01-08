7c70d5d0f4faf785f553ac7edbb20ca12a9bf353
Wissem <kiwan2005@gmail.com>
2023-01-08 09:51:59 +0100

    Create flyway.sh

---
+++ flyway.sh
@@ -0,0 +1,6 @@
+set -e
+jq -n "$DBT_SA" > /tmp/credentials.json
+export KEYFILE=/tmp/credentials.json
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials.json
+export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC42/1.0/GoogleBigQueryJDBC42-1.0.jar
+java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar
