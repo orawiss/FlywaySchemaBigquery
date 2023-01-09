0fd4dcf24d385da1b82144ed3ea7992fd1374b6c
orawiss <kiwan2005@gmail.com>
2023-01-09 18:25:35 +0100

    new doc version

--- flyway.sh
+++ flyway.sh
@@ -2,6 +2,7 @@
 set -e
 /usr/local/bin/jq -n "$FLYWAY_SA" > /tmp/credentials.json
 ##echo "$FLYWAY_SA"
+export KEYFILE=/tmp/credentials.json
 cat /tmp/credentials.json
 export GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials.json
 jar tvf /flyway.jar | grep migration
