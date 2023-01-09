76f3a635b3164f850cc9395b40c8fbffec3a2e9a
orawiss <kiwan2005@gmail.com>
2023-01-09 12:44:46 +0100

    change sh

--- flyway.sh
+++ flyway.sh
@@ -4,6 +4,5 @@ echo "$FLYWAY_SA"
 export KEYFILE=/tmp/credentials.json
 export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC42/1.0/GoogleBigQueryJDBC42-1.0.jar
 cp /FlywaySchemaBigquery/flyway.conf $HOME
-chmod -R 777 /FlywaySchemaBigquery/flyway/db/migration/
 ls -rtl /FlywaySchemaBigquery/flyway/db/migration/
 java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar
