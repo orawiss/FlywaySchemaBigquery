b2bde125a73e1e377f718a93fb1e3c9acba1b206
orawiss <kiwan2005@gmail.com>
2023-01-09 13:58:54 +0100

    new doc version

--- flyway.sh
+++ flyway.sh
@@ -5,4 +5,5 @@ export KEYFILE=/tmp/credentials.json
 export CLASSPATH=/FlywaySchemaBigquery/lib/GoogleBigQueryJDBC/GoogleBigQueryJDBC42/1.0/GoogleBigQueryJDBC42-1.0.jar
 cp /FlywaySchemaBigquery/flyway.conf $HOME
 ls -rtl /FlywaySchemaBigquery/flyway/db/migration/
+jar tvf /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar
 java -jar /FlywaySchemaBigquery/target/FlywaySchemaBigquery-1.0-SNAPSHOT-jar-with-dependencies.jar
