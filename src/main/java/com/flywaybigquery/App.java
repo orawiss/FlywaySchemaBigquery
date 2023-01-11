bbed1350c63b34947264f004c4c96eb0c9154513
orawiss <kiwan2005@gmail.com>
2023-01-11 13:18:26 +0100

    app

--- src/main/java/com/flywaybigquery/App.java
+++ src/main/java/com/flywaybigquery/App.java
@@ -8,30 +8,34 @@ import org.flywaydb.core.api.Location;
 
 public class App {
     public static void main(String[] args) {
+        try {
+            String flywayLocation = null;
+            if (args.length > 0) {
+                flywayLocation = args[0];
+            }
 
-        String flywayLocation = null;
-        if (args.length > 0) {
-            flywayLocation = args[0];
+            if (flywayLocation == null) {
+                flywayLocation = "/home/wissemk/IdeaProjects/FlywaySchemaBigquery/flyway/db/migration/";
+            }
+            System.out.println("flywayLocation:::"+flywayLocation+"\n");
+
+            DataSource dataSource = new DataSource();
+            dataSource.setURL("jdbc:bigquery://https://www.googleapis.com/bigquery/v2:443;ProjectId=data-product-analytic-ecommrce;OAuthType=3");
+
+            Flyway flyway = Flyway.configure()
+                    .createSchemas(false)
+                    .defaultSchema("data-product-analytic-ecommrce.flyway")
+                    .schemas("data-product-analytic-ecommrce.flyway")
+                    .dataSource(dataSource)
+                    .baselineOnMigrate(true)
+                    .locations("filesystem:"+flywayLocation)
+                    .load();
+
+            flyway.migrate();
+        }  catch (Exception e) {
+            System.err.println("Error running database migration: " + e.getMessage());
+            e.printStackTrace();
         }
-
-        if (flywayLocation == null) {
-            flywayLocation = "/home/wissemk/IdeaProjects/FlywaySchemaBigquery/flyway/db/migration/";
-        }
-        System.out.println("flywayLocation:::"+flywayLocation+"\n");
-
-        DataSource dataSource = new DataSource();
-        dataSource.setURL("jdbc:bigquery://https://www.googleapis.com/bigquery/v2:443;ProjectId=data-product-analytic-ecommrce;OAuthType=3");
-
-        Flyway flyway = Flyway.configure()
-                .createSchemas(false)
-                .defaultSchema("data-product-analytic-ecommrce.flyway")
-                .schemas("data-product-analytic-ecommrce.flyway")
-                .dataSource(dataSource)
-                .baselineOnMigrate(true)
-                .locations("filesystem:"+flywayLocation)
-                .load();
-
-        flyway.migrate();
-
     }
+
 }
